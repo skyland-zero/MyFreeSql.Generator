4401951d534a812dbbcdb571998548bb25ae9fb7
pengjianlin <skyland-zero@outlook.co>
2025-06-20 22:31:31 +0800

    修改 更新包版本

--- RazorModel.cs
+++ RazorModel.cs
@@ -263,7 +263,6 @@ public class RazorModel
                     case DataType.PostgreSQL:
                     case DataType.OdbcPostgreSQL:
                     case DataType.KingbaseES:
-                    case DataType.OdbcKingbaseES:
                     case DataType.ShenTong:
                         switch (col.DbTypeTextFull.ToLower())
                         {
@@ -288,7 +287,6 @@ public class RazorModel
                         }
                         break;
                     case DataType.Dameng:
-                    case DataType.OdbcDameng:
                         switch (col.DbTypeTextFull.ToLower())
                         {
                             case "text": sb.Add("StringLength = -2"); break;
@@ -371,7 +369,7 @@ public class RazorModel
         else if ((cstype == typeof(string) && defval.StartsWith("'") && defval.EndsWith("'::character varying") ||
                   cstype == typeof(Guid) && defval.StartsWith("'") && defval.EndsWith("'::uuid")
                  ) && (fsql.Ado.DataType == DataType.PostgreSQL || fsql.Ado.DataType == DataType.OdbcPostgreSQL ||
-                       fsql.Ado.DataType == DataType.KingbaseES || fsql.Ado.DataType == DataType.OdbcKingbaseES ||
+                       fsql.Ado.DataType == DataType.KingbaseES || 
                        fsql.Ado.DataType == DataType.ShenTong))
         {
             defval = defval.Substring(1, defval.LastIndexOf("'::") - 1).Replace("''", "'");
